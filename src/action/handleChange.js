ded911f7313e2e7a3cb9615dfec688190fd28a7f
amrz <theaxolagabe@gmail.com>
2019-10-18 20:12:25 -0400

    Apply Code promo  using Redux

--- src/action/handleChange.js
+++
@@ -1,7 +0,0 @@
-import { CODE_PROMO_ONCHANGE } from "./types/type";
-export default function handleChange(e) {
-  return {
-    type: CODE_PROMO_ONCHANGE,
-    payload: e.target.value
-  };
-}
--- src/action/handleSubmit.js
+++
@@ -1,7 +0,0 @@
-import { CODE_PROMO_ONSUBMIT } from "./types/type";
-export default function handleSubmit(e) {
-  return {
-    type: CODE_PROMO_ONSUBMIT,
-    payload: e.target.value
-  };
-}
